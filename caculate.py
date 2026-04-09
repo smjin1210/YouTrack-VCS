53453e303f286008692fc8a7b7931b39cba6d031
unknown <smjin@tangunsoft.com>
2026-04-09 15:04:09 +0900

    SamsungDX-159 PM 님 커밋완료하였습니다.2026.04.09

--- caculate.py
+++ caculate.py
@@ -15,4 +15,4 @@ elif operator == '-': print(subtract(num1, num2))
 elif operator == '*': print(multiply(num1, num2))
 elif operator == '/': print(divide(num1, num2))
 else: print("잘못된 연산자")
-#PM님 확인 부탁드립니다.
+#PM님 확인 부탁드립니다. 2026.04.09
