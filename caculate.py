f16e08c44860983720a16dfd6e09fabc06b1a504
unknown <smjin@tangunsoft.com>
2026-06-08 17:32:00 +0900

    Issue-53 Development Team Lead, I have completed the code modification. 2026.06.08

--- caculate.py
+++ caculate.py
@@ -1,6 +1,7 @@
-#Development Team Lead, please review this. 2026.06.07
+#Development Team Lead, please review this. 2026.06.08
 
-
+import ast
+import math
 import tkinter as tk
 from tkinter import ttk, messagebox
 
